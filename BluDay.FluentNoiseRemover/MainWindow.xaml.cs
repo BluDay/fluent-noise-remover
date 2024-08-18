23726adfb1513fee6b4423a222406c9d7d74bdac
BluDay <19403893+BluDay@users.noreply.github.com>
2024-08-18 16:38:08 +0200

    feat: add project files

---
+++ BluDay.FluentNoiseRemover/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+namespace BluDay.FluentNoiseRemover;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow() => InitializeComponent();
+}
---
+++ BluDay.FluentNoiseRemover/Usings.cs
@@ -0,0 +1,6 @@
+﻿global using BluDay.FluentNoiseRemover;
+
+global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
+
+global using Windows.Graphics;
