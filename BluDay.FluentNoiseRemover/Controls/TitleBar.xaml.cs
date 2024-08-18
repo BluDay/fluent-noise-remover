04ad439b08b67b59058588f916f1c0cd1a85ed6c
BluDay <19403893+BluDay@users.noreply.github.com>
2024-08-18 17:33:04 +0200

    feat: add new `TitleBar` control

---
+++ BluDay.FluentNoiseRemover/Controls/TitleBar.xaml.cs
@@ -0,0 +1,12 @@
+namespace BluDay.FluentNoiseRemover.Controls;
+
+/// <summary>
+/// Interaction logic for TitleBar.xaml.
+/// </summary>
+public sealed partial class TitleBar : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TitleBar"/> class.
+    /// </summary>
+    public TitleBar() => InitializeComponent();
+}
